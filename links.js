88c5cdaad8b7c4329915517666d8dbd74959d27b
ivansglazunov <ivansglazunov@gmail.com>
2015-12-23 06:13:42 +0300

    0.0.2

--- links.js
+++ links.js
@@ -2,16 +2,11 @@
 // https://github.com/ivansglazunov/meteor-links/issues/1
 LinkSimpleSchema = new SimpleSchema({
   link: {
-    type: Object,
+    type: DBRefSchema,
     optional: true
   },
   type: {
     type: String
-    optional: true
-  },
-  package: {
-    type: String
-    optional: true
   }
 });
 LinksSimpleSchema = new SimpleSchema({
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ivansglazunov:links',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Typed links with DBRef.',
   git: 'https://github.com/ivansglazunov/meteor-links',
   documentation: 'README.md'
@@ -10,6 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('mongo');
   api.use('aldeed:simple-schema@1.5.1');
+  api.use('ivansglazunov:dbrefs@0.0.4');
   api.use('ecmascript');
   api.addFiles('links.js');
   api.export('LinksSimpleSchema');
